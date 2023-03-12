7658f2cb5cd743dc6e2fcefc2d21c87bf57f21b6
Konstantin Babushkin <97439218+kogrms@users.noreply.github.com>
2023-03-12 18:29:42 +0300

    add models & schemas

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
-
-const app = express();
+const mongoose = require('mongoose');
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!');
 // });
 const port = process.env.PORT || 3000;
+const url = 'mongodb://localhost:27017/mestodb';
+
+mongoose.connect(url);
+// , {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// }).then(() => {
+//   console.log('Connected to MongoDB');
+//   app.listen(port, () => {
+//     console.log(`Server is listening on port ${port}...`);
+//   });
+// }).catch((err) => {
+//   console.error('Failed to connect to MongoDB:', err);
+// });
+
+const app = express();
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}...`);
