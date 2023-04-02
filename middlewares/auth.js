4d699a96a800f629a9660eda88e150a753f40105
Konstantin Babushkin <97439218+kogrms@users.noreply.github.com>
2023-04-02 23:44:35 +0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,5 @@
 const jwt = require('jsonwebtoken');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
-
 module.exports = (req, res, next) => {
   if (!req.cookies.jwt) {
     return res.status(401).send({ message: 'Необходима авторизация' });
@@ -10,7 +8,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(req.cookies.jwt, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(req.cookies.jwt, 'dev-secret');
   } catch (err) {
     return res.status(401).send({ message: 'Необходима авторизация' });
   }
