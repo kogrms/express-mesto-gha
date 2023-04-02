330eee64b8a9fd0bd2bc52987d7b0c6078a0d8dc
Konstantin Babushkin <97439218+kogrms@users.noreply.github.com>
2023-04-02 14:50:23 +0300

    Add Validator & upd userSchema

--- models/user.js
+++ models/user.js
@@ -1,22 +1,41 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    default: 'Жак-Ив Кусто',
     minlength: 2,
-    maxlength: 30
+    maxlength: 30,
   },
   about: {
     type: String,
-    required: true,
+    default: 'Исследователь',
     minlength: 2,
-    maxlength: 30
+    maxlength: 30,
   },
   avatar: {
     type: String,
-    required: true
-  }
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator: validator.isURL,
+      message: 'Некорректный URL',
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: 'Некорректный email',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
 });
 userSchema.set('versionKey', false);
 
