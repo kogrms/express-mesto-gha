53455ebfb4672c9aa4ddc014b8eff6665c05e50e
Konstantin Babushkin <97439218+kogrms@users.noreply.github.com>
2023-04-09 14:52:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
 require('dotenv').config();
 const {
   celebrate, Joi, Segments, errors,
