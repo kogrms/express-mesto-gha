2c02d300387ed8479c09a28fac8bf9dd07cae51e
kogrms <kostya-babushkin@yandex.ru>
2023-04-15 22:41:37 +0300

    upd

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 require('dotenv').config();
+const cors = require('cors');
 const {
   celebrate, Joi, Segments, errors,
 } = require('celebrate');
@@ -13,9 +14,10 @@ const { postUsers, login } = require('./controllers/users');
 const { STATUS_500 } = require('./utils/constants');
 
 const { MONGOOSE_ENV = 'mongodb://localhost:27017/mestodb' } = process.env;
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
